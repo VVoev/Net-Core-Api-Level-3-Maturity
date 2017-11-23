6f1a8a7f4fa9e355ea4ccfcb27722795b869cd81
VVoev <vladovoev@yahoo.co.uk>
2017-11-23 16:39:55 +0200

    initial commit

---
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -0,0 +1,26 @@
+﻿using Library.API.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Library.API.Controllers
+{
+    public class AuthorsController : Controller
+    {
+        private ILibraryRepository repository;
+
+        public AuthorsController(ILibraryRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        [HttpGet("api/authors")]
+        public IActionResult GetResults()
+        {
+            var authors = repository.GetAuthors();
+            return new JsonResult(authors);
+        }
+    }
+}
