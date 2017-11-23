37af655dfdc39a9ff3e6d53db835d77419df2529
VVoev <vladovoev@yahoo.co.uk>
2017-11-23 17:41:44 +0200

    added first controlller ,Automapper,and a couple of get requests

--- src/Library.API/Controllers/AuthorsController.cs
+++ src/Library.API/Controllers/AuthorsController.cs
@@ -1,4 +1,7 @@
-﻿using Library.API.Services;
+﻿using AutoMapper;
+using Library.API.Helpers;
+using Library.API.Models;
+using Library.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -7,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace Library.API.Controllers
 {
+    [Route("api/authors")]
     public class AuthorsController : Controller
     {
         private ILibraryRepository repository;
@@ -16,11 +20,21 @@ namespace Library.API.Controllers
             this.repository = repository;
         }
 
-        [HttpGet("api/authors")]
+        [HttpGet]
         public IActionResult GetResults()
         {
-            var authors = repository.GetAuthors();
+            var authorsFromRepo = repository.GetAuthors();
+            var authors = Mapper.Map<IEnumerable<AuthorDto>>(authorsFromRepo);
             return new JsonResult(authors);
         }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthorById(Guid id)
+        {
+            var authorWithCorrectId = repository.GetAuthor(id);
+            var mappedAuthor = Mapper.Map<AuthorDto>(authorWithCorrectId);
+            return new JsonResult(mappedAuthor);
+        }
+
     }
 }
